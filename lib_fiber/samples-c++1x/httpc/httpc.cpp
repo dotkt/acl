7a88a1e744017937783a8be699f5c1b35b42a48e
dotkt <holiday_life@outlook.com>
2023-03-22 16:04:30 +0800

    协程类的TCP服务器

--- lib_fiber/samples-c++1x/httpc/httpc.cpp
+++ lib_fiber/samples-c++1x/httpc/httpc.cpp
@@ -1,125 +1,85 @@
-#include "stdafx.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <acl_cpp/lib_acl.hpp>
+#include <fiber/libfiber.hpp>
 
-//////////////////////////////////////////////////////////////////////////////
-#include <iostream>
-#include <chrono>
-using namespace std;
 
-static int start(const char* addr, int count) {
-	int i;
-	acl::string buf;
-
-	for (i = 0; i < count; i++) {
-		acl::http_request req(addr);
-		acl::http_header& head = req.request_header();
-		head.set_url("/")
-			.accept_gzip(false)
-			.set_host(addr)
-			.set_keep_alive(false);
-
-		acl::string tmp;
-		head.build_request(tmp);
-		printf("threadi:%d>>request: %s\r\n",GetCurrentThreadId(), tmp.c_str());
-		auto start = std::chrono::high_resolution_clock::now(); // 记录开始时间
-		if (!req.request(NULL, 0)) {
-			printf("request error\r\n");
-			break;
+// 客户端协程处理类，用来回显客户发送的内容，每一个客户端连接绑定一个独立的协程
+class fiber_echo : public acl::fiber
+{
+public:
+	fiber_echo(acl::socket_stream* conn) : conn_(conn) {}
+private:
+	acl::socket_stream* conn_;
+	~fiber_echo(void) { delete conn_; }
+	// @override
+	void run(void) {
+		char buf[8192];
+		while (true) {
+			// 从客户端读取数据（第三个参数为false表示不必填满整个缓冲区才返回）
+			int ret = conn_->read(buf, sizeof(buf), false);
+			if (ret == -1) {
+				break;
+			}
+			// 向客户端写入读到的数据
+			if (conn_->write(buf, ret) != ret) {
+				break;
+			}
 		}
-		auto end = std::chrono::high_resolution_clock::now(); // 记录结束时间
-		std::chrono::duration<double> diff = end - start; // 计算时间差
-		std::cout << "Code running time: " << diff.count() << " s\n"; // 输出运行时间，单位为秒
-		//printf(">>>begin read body\r\n");
-		
-		if (!req.get_body(buf)) {
-			printf("get response error\r\n");
-			break;
-		}
-
-		if (i == 0) {
-			printf("response: %s\r\n", buf.c_str());
-		}
-		buf.clear();
+		delete this; // 自销毁动态创建的协程对象
 	}
-	return i;
-}
+};
 
-//////////////////////////////////////////////////////////////////////////////
-
-static void usage(const char* procname) {
-	printf("usage: %s -h [help]\r\n"
-		" -e event_type[kernel|select|poll, default: kernel]\r\n"
-		" -s server_addr[default: 127.0.0.1:8088]\r\n"
-		" -c fiber_count[default: 1]\r\n"
-		" -n count[default: 100]\r\n"
-		, procname);
-}
-
-int main(int argc, char *argv[]) {
-	int  ch, nfiber = 1, count = 30;
-	acl::string addr = "127.0.0.1:8088", event_type("kernel");
-	printf("current threadid:%d\n",GetCurrentThreadId());
-	acl::acl_cpp_init();
-	acl::log::stdout_open(true);
-
-	while ((ch = getopt(argc, argv, "he:s:c:n:")) > 0) {
-		switch (ch) {
-		case 'h':
-			usage(argv[0]);
-			return 0;
-		case 'e':
-			event_type = optarg;
-			break;
-		case 's':
-			addr = optarg;
-			break;
-		case 'c':
-			nfiber = atoi(optarg);
-			break;
-		case 'n':
-			count = atoi(optarg);
-			break;
-		default:
-			break;
+// 独立的协程过程，接收客户端连接，并将接收的连接与新创建的协程进行绑定
+class fiber_listen : public acl::fiber
+{
+public:
+	fiber_listen(acl::server_socket& listener) : listener_(listener) {}
+private:
+	acl::server_socket& listener_;
+	~fiber_listen(void) {}
+	// @override
+	void run(void) {
+		while (true) {
+			acl::socket_stream* conn = listener_.accept();  // 等待客户端连接
+			if (conn == NULL) {
+				printf("accept failed: %s\r\n", acl::last_serror());
+				break;
+			}
+			// 创建并启动单独的协程处理客户端连接
+			acl::fiber* fb = new fiber_echo(conn);
+			// 启动独立的客户端处理协程
+			fb->start();
 		}
+		delete this;
+	}
+};
+
+int main(void)
+{
+    // Initialize Winsock
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0)
+    {
+        printf("WSAStartup failed: %d\n", result);
+        return 1;
+    }
+
+    // Use Windows Sockets API here...
+
+
+	const char* addr = "127.0.0.1:8800";
+	acl::server_socket listener;
+	// 监听本地地址
+	if (listener.open(addr) == false) {
+		printf("listen %s error %s\r\n", addr, acl::last_serror());
+		return 1;
 	}
 
-	acl::fiber_event_t type;
+	// 创建并启动独立的监听协程，接受客户端连接
+	acl::fiber* fb = new fiber_listen(listener);
+	fb->start();
 
-	if (event_type == "select") {
-		type = acl::FIBER_EVENT_T_SELECT;
-	} else if (event_type == "poll") {
-		type = acl::FIBER_EVENT_T_POLL;
-	} else {
-		type = acl::FIBER_EVENT_T_KERNEL;
-	}
-
-	long long total = 0;
-
-	struct timeval begin;
-	gettimeofday(&begin, NULL);
-
-	//for (int i = 0; i < nfiber; i++) {
-	//	go[&] {
-	//		total += start(addr, count);
-	//	};
-	//}
-
-	go_wait_fiber []{
-		start("www.baidu.com",1);
-	};
-
-	go_wait_fiber []{
-		start("www.163.com",1);
-	};
-
-	acl::fiber::schedule_with(type);
-
-	struct timeval end;
-	gettimeofday(&end, NULL);
-	double cost = acl::stamp_sub(end, begin);
-	printf("Total count=%lld, cost=%.2f ms, speed=%.2f\r\n",
-		total, cost, (total * 1000) / (cost > 0 ? cost : 0.1));
+	// 启动协程调度器
+	acl::fiber::schedule();
 	return 0;
 }
